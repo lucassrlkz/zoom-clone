a86965f8b9f716242a8eda743221dd78eb0b08aa
LKZ <lucas_vieira97@hotmail.com>
2021-01-28 10:52:36 -0300

    js-expert second-class
    add peer stream and multiple users

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -15,6 +15,17 @@ const onload = () => {
   // recorderBtn.addEventListener('click', recordClick(recorderBtn))
   const socketUrl = 'http://localhost:3000'
   const socketBuilder = new SocketBuilder({ socketUrl })
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/',
+    },
+  })
+  const peerBuilder = new PeerBuilder({ peerConfig })
+
   const view = new View()
   const media = new Media()
   const deps = {
@@ -22,6 +33,7 @@ const onload = () => {
     media,
     room,
     socketBuilder,
+    peerBuilder,
   }
 
   Business.initialize(deps)
---
+++ public/pages/room/src/util/peer.js
@@ -0,0 +1,50 @@
+class PeerBuilder {
+  constructor({ peerConfig }) {
+    const defaultFunctionValue = () => {}
+    this.peerConfig = peerConfig
+    this.onError = defaultFunctionValue
+    this.onCallReceived = defaultFunctionValue
+    this.onConnectionOpened = defaultFunctionValue
+    this.onPeerStreamReceived = defaultFunctionValue
+  }
+
+  setOnError(fn) {
+    this.onError = fn
+    return this
+  }
+
+  setOnCallReceived(fn) {
+    this.onCallReceived = fn
+    return this
+  }
+
+  setOnConnectionOpened(fn) {
+    this.onConnectionOpened = fn
+    return this
+  }
+
+  setOnPeerStreamReceived(fn) {
+    this.onPeerStreamReceived = fn
+    return this
+  }
+
+  _prepareCallEvent(call) {
+    call.on('stream', (stream) => this.onPeerStreamReceived(call, stream))
+
+    this.onCallReceived(call)
+  }
+
+  build() {
+    const peer = new Peer(...this.peerConfig)
+
+    peer.on('error', this.onError)
+    peer.on('call', this._prepareCallEvent.bind(this))
+
+    return new Promise((res) =>
+      peer.on('open', (id) => {
+        this.onConnectionOpened(peer)
+        return res(peer)
+      })
+    )
+  }
+}
